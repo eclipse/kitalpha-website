6d7eed0695bed2a30776f1fe7be6993b99b11cb0
Philippe DUL <philippe.dul@thalesgroup.com>
2022-09-14 15:14:37 +0200

    Fix navigation header

--- js/nav.js
+++ js/nav.js
@@ -1,2 +1,2 @@
 let links = [...document.getElementsByTagName("nav")[0].getElementsByTagName("a")];
-links.filter(x => "/" + x.getAttribute("href") == document.location.pathname).forEach(e => e.parentNode.setAttribute("class", "selected"));
+links.filter(x => document.location.pathname.endsWith("/" + x.getAttribute("href"))).forEach(e => e.parentNode.setAttribute("class", "selected"));
